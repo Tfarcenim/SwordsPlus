e0fb76676c8b5aa932276cbc36369a16f99d9359
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2020-07-18 13:37:23 -0400

    1.2

--- src/main/java/tfar/swordsplus/sword/ArturiaExcelsionItem.java
+++ src/main/java/tfar/swordsplus/sword/ArturiaExcelsionItem.java
@@ -1,8 +1,11 @@
 package tfar.swordsplus.sword;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ArturiaExcelsionItem extends CaliburSwordItem {
 	public ArturiaExcelsionItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builder, int regenRate) {
@@ -12,5 +15,10 @@ public class ArturiaExcelsionItem extends CaliburSwordItem {
 	@Override
 	public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 		return stack.getDamage() < stack.getMaxDamage() - 1 && super.hitEntity(stack, target, attacker);
+	}
+
+	@Override
+	public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+		return stack.getDamage() < stack.getMaxDamage() - 2 && super.onBlockDestroyed(stack, worldIn, state, pos, entityLiving);
 	}
 }
